c7802523609017f2dabbcf8b2e4c983b81442a87
shuaihan <1034683568@qq.com>
2017-04-02 11:30:32 +0800

    run.sh 修改

--- run.sh
+++ run.sh
@@ -10,9 +10,13 @@ mvn clean install -Dmaven.test.skip=true
 # 停止原先运行的容器
 docker-compose stop
 docker-compose rm -f
+#删除停止的容器
+docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
+#删除名称为none的镜像
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
 
 # 使用docker-compose启动多容器应用
-docker-compose up -d
+docker-compose up  -d
 
 # 日志
 docker-compose logs
