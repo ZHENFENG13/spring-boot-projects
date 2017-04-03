a5c982292b5c4fc3a23ef9c68d295c12e7262ba6
shuaihan <1034683568@qq.com>
2017-04-03 11:25:29 +0800

    mysql用户权限处理

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 echo `service mysql status`
-service mysql start
-sleep 10
-echo `service mysql status`
+#启动mysql
 service mysql start
 sleep 10
 echo `service mysql status`
 #导入数据
 mysql < /mysql/schema.sql
+#停止mysql
+service mysql stop
+#以不启用grant-tables模式启动mysql
+mysqld_safe --skip-grant-tables &
+echo `service mysql status`
 #重新设置mysql密码
-mysqladmin -u root password '123456'
+mysql < /mysql/editpassword.sql
+service mysql start
+sleep 10
+echo `service mysql status`
