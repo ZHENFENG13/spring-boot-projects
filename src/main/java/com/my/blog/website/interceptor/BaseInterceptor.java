dc4ce8357db7e41ea592644101ff41c2bfb4e50d
shuaihan <1034683568@qq.com>
2017-04-16 15:16:51 +0800

    upd

--- src/main/java/com/my/blog/website/interceptor/BaseInterceptor.java
+++ src/main/java/com/my/blog/website/interceptor/BaseInterceptor.java
@@ -76,7 +76,7 @@ public class BaseInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-        httpServletRequest.setAttribute("commons", commons);
+        httpServletRequest.setAttribute("commons", commons);//一些工具类和公共方法
         httpServletRequest.setAttribute("adminCommons", adminCommons);
     }
 
