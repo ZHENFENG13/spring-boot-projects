4018374ae1f1ebb47ccc0e74b35e9ecb6ba36639
shuaihan <1034683568@qq.com>
2017-05-09 11:09:41 +0800

    mysql5.7镜像中,mysql服务器的创建过程及数据导入过程重写

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 set -e
+
 echo `service mysql status`
+
+echo '1.启动mysql....'
 #启动mysql
 service mysql start
-sleep 10
+
+sleep 3
 echo `service mysql status`
+echo '2.开始导入数据....'
 #导入数据
 mysql < /mysql/schema.sql
+echo '3.导入数据完毕....'
 #停止mysql
-service mysql stop
-#以不启用grant-tables模式启动mysql
-mysqld_safe --skip-grant-tables &
+sleep 3
 echo `service mysql status`
 #重新设置mysql密码
+echo '4.开始修改密码....'
 mysql < /mysql/editpassword.sql
-service mysql start
-sleep 10
+echo '5.修改密码完毕....'
+
+#echo '6.重启mysql....'
+#service mysql stop
+#service mysql start
+
+#sleep 3
 echo `service mysql status`
+echo `mysql容器启动完毕,且数据导入成功`
+
+tail -f /dev/null
