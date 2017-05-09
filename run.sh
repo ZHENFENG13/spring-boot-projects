47a499fc292f7a30b6c9b08103855f828ebdab26
shuaihan <1034683568@qq.com>
2017-05-09 12:24:03 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,19 @@
 
 set -e
 
-# 打包并创建镜像
-mvn clean install -Dmaven.test.skip=true
-#如果已经创建好镜像,这一步要注释掉,避免重复操作(不注释也可以)
+#如果已经创建好镜像,这一步要注释掉,避免重复操作
 #mvn clean install -Dmaven.test.skip=true
+
+# 打包并创建镜像(不注释也可以,镜像覆盖,因此需要删除原有镜像,命令在下面,删除名称为none的镜像)
+mvn clean install -Dmaven.test.skip=true
 
 # 停止原先运行的容器
 docker-compose stop
 docker-compose rm -f
+
 #删除停止的容器
 #docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
+
 #删除名称为none的镜像
 docker images|grep none|awk '{print $3 }'|xargs docker rmi
 
@@ -19,4 +22,4 @@ docker images|grep none|awk '{print $3 }'|xargs docker rmi
 docker-compose up --build -d
 
 # 日志
-docker-compose logs
+docker-compose logs -f
