f3836a35f87ff8ecf0517f17926c7b45b231c5a0
shuaihan <1034683568@qq.com>
2017-05-16 12:22:27 +0800

    无用代码删除

--- src/main/java/com/my/blog/website/utils/TaleUtils.java
+++ src/main/java/com/my/blog/website/utils/TaleUtils.java
@@ -68,72 +68,6 @@ public class TaleUtils {
     }
 
     /**
-     * 获取当前时间
-     *
-     * @return
-     */
-    public static int getCurrentTime() {
-        return (int) (new Date().getTime() / 1000);
-    }
-
-    /**
-     * jdbc:mysql://127.0.0.1:3306/tale?useUnicode=true&characterEncoding=utf-8&useSSL=false 保存jdbc数据到文件中
-     *
-     * @param url      数据库连接地址 127.0.0.1:3306
-     * @param dbName   数据库名称
-     * @param userName 用户
-     * @param password 密码
-     */
-    public static void updateJDBCFile(String url, String dbName, String userName, String password) {
-        LOGGER.info("Enter updateJDBCFile method");
-        Properties props = new Properties();
-        FileOutputStream fos = null;
-
-        try {
-            fos = new FileOutputStream("application-jdbc.properties");
-            props.setProperty("spring.datasource.url", url);
-            props.setProperty("spring.datasource.dbname", dbName);
-            props.setProperty("spring.datasource.username", userName);
-            props.setProperty("spring.datasource.password", password);
-            props.setProperty("spring.datasource.driver-class-name", "com.mysql.jdbc.Driver");
-            props.store(fos, "update jdbc info.");
-        } catch (IOException e) {
-            LOGGER.error("updateJDBCFile method fail:{}", e.getMessage());
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        LOGGER.info("Exit updateJDBCFile method");
-    }
-
-    /**
-     * 获取properties配置数据,
-     *
-     * @param fileName 文件名 如 application-jdbc.properties来自jar中
-     * @return
-     */
-    public static Properties getPropFromJar(String fileName) {
-        Properties properties = new Properties();
-        try {
-//            默认是classPath路径
-            InputStream resourceAsStream = TaleUtils.class.getClassLoader().getResourceAsStream(fileName);
-            if (resourceAsStream == null) {
-                throw new TipException("get resource from path fail");
-            }
-            properties.load(resourceAsStream);
-        } catch (TipException | IOException e) {
-            LOGGER.error("get properties file fail={}", e.getMessage());
-        }
-        return properties;
-    }
-
-    /**
      * @param fileName 获取jar外部的文件
      * @return 返回属性
      */
@@ -189,7 +123,6 @@ public class TaleUtils {
                     return newDataSource;
                 }
                 DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
-                //        TODO 对不同数据库支持
                 managerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
                 managerDataSource.setPassword(properties.getProperty("spring.datasource.password"));
                 String str = "jdbc:mysql://" + properties.getProperty("spring.datasource.url") + "/" + properties.getProperty("spring.datasource.dbname") + "?useUnicode=true&characterEncoding=utf-8&useSSL=false";
@@ -302,15 +235,6 @@ public class TaleUtils {
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         String content = renderer.render(document);
         content = Commons.emoji(content);
-
-        // TODO 支持网易云音乐输出
-//        if (TaleConst.BCONF.getBoolean("app.support_163_music", true) && content.contains("[mp3:")) {
-//            content = content.replaceAll("\\[mp3:(\\d+)\\]", "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=350 height=106 src=\"//music.163.com/outchain/player?type=2&id=$1&auto=0&height=88\"></iframe>");
-//        }
-        // 支持gist代码输出
-//        if (TaleConst.BCONF.getBoolean("app.support_gist", true) && content.contains("https://gist.github.com/")) {
-//            content = content.replaceAll("&lt;script src=\"https://gist.github.com/(\\w+)/(\\w+)\\.js\">&lt;/script>", "<script src=\"https://gist.github.com/$1/$2\\.js\"></script>");
-//        }
         return content;
     }
 
