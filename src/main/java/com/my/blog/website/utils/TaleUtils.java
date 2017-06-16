d8598a33626693a67f632be66cef0dcd5a043b93
shuaihan <1034683568@qq.com>
2017-06-16 12:52:19 +0800

    fix bug:markdown表格样式错误

--- src/main/java/com/my/blog/website/utils/TaleUtils.java
+++ src/main/java/com/my/blog/website/utils/TaleUtils.java
@@ -5,6 +5,8 @@ import com.my.blog.website.constant.WebConst;
 import com.my.blog.website.controller.admin.AttachController;
 import com.my.blog.website.modal.Vo.UserVo;
 import org.apache.commons.lang3.StringUtils;
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -23,6 +25,7 @@ import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.Normalizer;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Properties;
 import java.util.regex.Matcher;
@@ -200,7 +203,7 @@ public class TaleUtils {
             boolean isSSL = false;
             Cookie cookie = new Cookie(WebConst.USER_IN_COOKIE, val);
             cookie.setPath("/");
-            cookie.setMaxAge(60*30);
+            cookie.setMaxAge(60 * 30);
             cookie.setSecure(isSSL);
             response.addCookie(cookie);
         } catch (Exception e) {
@@ -231,8 +234,10 @@ public class TaleUtils {
         if (StringUtils.isBlank(markdown)) {
             return "";
         }
+        java.util.List<Extension> extensions = Arrays.asList(TablesExtension.create());
+        Parser parser = Parser.builder().extensions(extensions).build();
         Node document = parser.parse(markdown);
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
         String content = renderer.render(document);
         content = Commons.emoji(content);
         return content;
