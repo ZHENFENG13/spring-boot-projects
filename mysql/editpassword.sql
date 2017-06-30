cfcae1d43bc8a54ec372c20c2cfd5d6d77e73e7e
shuaihan <1034683568@qq.com>
2017-06-30 10:16:23 +0800

    :fire:docker:mysql

--- mysql/editpassword.sql
+++
@@ -1,9 +0,0 @@
-use mysql;
-
-select host, user from user;
-
-create user tale identified by '123456';
-
-grant all on tale.* to tale@'%' identified by '123456' with grant option;
-
-flush privileges;
