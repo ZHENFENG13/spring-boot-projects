0aa3eeae453c3ef4d6bb9ed7207b672a841b08ea
shuaihan <1034683568@qq.com>
2017-06-30 12:38:53 +0800

    :rocket:run.sh & docker-compose.yml

--- docker-extension/run.sh
+++ docker-extension/run.sh
@@ -10,9 +10,10 @@ cd ../
 # 打包并创建镜像(不注释也可以,镜像覆盖,因此需要删除原有镜像,命令在下面,删除名称为none的镜像)
 mvn clean install -Dmaven.test.skip=true
 
-# 停止原先运行的容器
+
 cd docker-extension
 
+# 停止原先运行的容器
 docker-compose stop
 docker-compose rm -f
 
