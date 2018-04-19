bd1bb1ef732bcd0b1994150bcc82006813aafee0
13 <1034683568@qq.com>
2018-04-19 16:52:53 +0800

    :fire::fire::fire:MapCache修改

--- src/main/java/com/my/blog/website/utils/MapCache.java
+++ src/main/java/com/my/blog/website/utils/MapCache.java
@@ -45,9 +45,14 @@ public class MapCache {
         CacheObject cacheObject = cachePool.get(key);
         if (null != cacheObject) {
             long cur = System.currentTimeMillis() / 1000;
+            //未过期直接返回
             if (cacheObject.getExpired() <= 0 || cacheObject.getExpired() > cur) {
                 Object result = cacheObject.getValue();
                 return (T) result;
+            }
+            //已过期直接删除
+            if (cur > cacheObject.getExpired()) {
+                cachePool.remove(key);
             }
         }
         return null;
@@ -85,6 +90,10 @@ public class MapCache {
      */
     public void set(String key, Object value, long expired) {
         expired = expired > 0 ? System.currentTimeMillis() / 1000 + expired : expired;
+        //cachePool大于800时，强制清空缓存池，这个操作有些粗暴会导致误删问题，后期考虑用redis替代MapCache优化
+        if (cachePool.size() > 800) {
+            cachePool.clear();
+        }
         CacheObject cacheObject = new CacheObject(key, value, expired);
         cachePool.put(key, cacheObject);
     }
