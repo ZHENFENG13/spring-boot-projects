38666bca18f8fc341de8e8fa7e4c81aaa3753591
涵 <2449207463@qq.com>
2018-09-27 17:29:53 +0800

    :bug: fix a bug

--- src/main/java/com/my/blog/website/utils/LogAspect.java
+++ src/main/java/com/my/blog/website/utils/LogAspect.java
@@ -12,23 +12,19 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.sql.DataSource;
 import java.util.Arrays;
 
-/**
- * 借口aop
- * Created by wangq on 2017/3/24.
- */
 @Aspect
 @Component
 public class LogAspect {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DataSource.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogAspect.class);
 
     @Pointcut("execution(public * com.my.blog.website.controller..*.*(..))")
-    public void webLog(){}
+    public void webLog() {
+    }
 
     @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable {
+    public void doBefore(JoinPoint joinPoint) {
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
@@ -37,7 +33,7 @@ public class LogAspect {
     }
 
     @AfterReturning(returning = "object", pointcut = "webLog()")
-    public void doAfterReturning(Object object) throws Throwable {
+    public void doAfterReturning(Object object) {
         // 处理完请求，返回内容
         LOGGER.info("RESPONSE : " + object);
     }
