2d9b242716837925041cff88629a2c54ac90310d
13 <1034683568@qq.com>
2019-03-11 17:16:54 +0800

    :tada: spring-boot-file-upload

---
+++ SpringBoot入门案例源码/spring-boot-file-upload/src/main/java/com/lou/springboot/config/SpringBootWebMvcConfigurer.java
@@ -0,0 +1,13 @@
+package com.lou.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringBootWebMvcConfigurer implements WebMvcConfigurer {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //registry.addResourceHandler("/files/**").addResourceLocations("file:D:\\upload\\");
+        registry.addResourceHandler("/files/**").addResourceLocations("file:/home/project/upload/");
+    }
+}
