68e2df07530385553ef11073a800b2ee2d0c41c5
13 <1034683568@qq.com>
2019-04-22 20:15:52 +0800

    :tada: Initial commit spring-boot-swagger

---
+++ 玩转SpringBoot系列案例源码/spring-boot-swagger/src/main/java/com/lou/springboot/config/Swagger2Config.java
@@ -0,0 +1,34 @@
+package com.lou.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lou.springboot.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("swagger-api文档")
+                .description("swagger文档 by 13")
+                .version("1.0")
+                .build();
+    }
+}
